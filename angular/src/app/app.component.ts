89125ab9fa32be6458f855393f262f74ef84cc6c
danny27028 <duyanh27082005@gmail.com>
2026-02-27 15:25:57 +0700

    Add NotificationService integration in AppComponent: establish real-time notification connection on initialization

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { DynamicLayoutComponent } from '@abp/ng.core';
 import { LoaderBarComponent } from '@abp/ng.theme.shared';
+import { NotificationService } from './shared/services/notification.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,13 @@ import { LoaderBarComponent } from '@abp/ng.theme.shared';
     <abp-loader-bar />
     <abp-dynamic-layout />
   `,
+  standalone: true,
   imports: [LoaderBarComponent, DynamicLayoutComponent],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly notificationService = inject(NotificationService);
+
+  ngOnInit(): void {
+    this.notificationService.startConnection();
+  }
+}
